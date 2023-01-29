c66c40ce737e969477101224c78845304d3024b3
Marc Jacques <39604945+MarcJacques@users.noreply.github.com>
2023-01-29 23:29:47 +0000

    adds iomanip to display precision up to the tenth

--- lab9.cpp
+++ lab9.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main() 
 {
   
+  const double DISTANCE_FACTOR= 0.5 * 9.8;
+  int fallTime, height;
+  double distanceFallen; 
+  
   cout << "Please input the time of fall in seconds:";
-  cin >> fallInSeconds;
+  cin >> fallTime;
 
   cout << "Please input the height of the bridge in meters:";
   cin >> height;
 
-  for (int  i = 0; i <= fallInSeconds; i++) {
-    
+  for (int  i = 0; i <= fallTime; i++) {
+    distanceFallen = (i * i) * DISTANCE_FACTOR;
+
+    cout << fixed << showpoint << setprecision(2);
+    cout << i << " " << distanceFallen << endl; 
   }
   return 0;
 }
