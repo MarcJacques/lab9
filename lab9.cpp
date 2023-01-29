9500027867f8b578de80128a86e2c3cb2e24614c
Marc Jacques <39604945+MarcJacques@users.noreply.github.com>
2023-01-29 23:14:52 +0000

    add output and input statements

--- lab9.cpp
+++ lab9.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+using namespace std;
 
-int main() {
-  std::cout << "Hello World!\n";
+int main() 
+{
+  
+  cout << "Please input the time of fall in seconds:";
+  cin >> fallInSeconds;
+
+  cout << "Please input the height of the bridge in meters:";
+  cin >> height;
+
+  for (int  i = 0; i <= fallInSeconds; i++) {
+    
+  }
+  return 0;
 }
